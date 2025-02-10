1167f5b6d7bfc0833bd5ffcf4ef68f51fadc6fc6
RaviChhetri <rabikc139@gmail.com>
2025-02-10 13:35:02 +0545

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Home from '.components/Home';
-import About from '.components/About';
-import Booking from '.components/Booking';
-import Contact from '.components/Contact';
+import Home from './components/Home';
+// import About from './components/About';
+// import Booking from './components/Booking';
+// import Contact from './components/Contact';
 
 function App() {
   return (
+    <Home/>
   );
 }
-
 export default App;
