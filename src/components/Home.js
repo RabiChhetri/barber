4e67565e340a06f36630925b011bb5e72747e424
RaviChhetri <rabikc139@gmail.com>
2025-02-10 18:21:38 +0545

    final navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,34 @@
 import React from 'react';
+import logo from '../background/final.png';
 
 export default function Home() {
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
+    <nav className="navbar navbar-expand-lg ">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">logo</a>
+      <a href="/">
+        <img src={logo} alt="Salon Logo" style={{height:'50px',width:'auto'}}/>
+      </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse mx-2" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
+            <a className="nav-link  text" href="/">Home</a>
           </li>
           <li className="nav-item ">
-            <a className="nav-link text-light" href="/">About Us</a>
+            <a className="nav-link text" href="/">About Us</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link  text-light" href="/">Contact Us</a>
+            <a className="nav-link  text" href="/">Contact Us</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link text-light" href="/">Book Appointment</a>
+            <a className="nav-link text" href="/">Book Appointment</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text" href="/">Log In</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
